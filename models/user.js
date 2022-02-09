f33a00bf00a5765bd18df62a0b8f2f30504bda47
Juan Mendez <kozincmusic@gmail.com>
2022-02-08 22:16:51 -0500

    Passport Facebook

--- models/user.js
+++ models/user.js
@@ -3,20 +3,23 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstname: {
-        type: String,
-        default: ''
-    },
-    lastname: {
-        type: String,
-        default: ''
-    },
-    admin: {
-        type: Boolean,
-        default: false
-    }
+    facebookId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
+
+//OLD USER SCHEMA OBJECTS
+// firstname: {
+//     type: String,
+//     default: ''
+// },
+// lastname: {
+//     type: String,
+//     default: ''
+// },
+// admin: {
+//     type: Boolean,
+//     default: false
+// }
